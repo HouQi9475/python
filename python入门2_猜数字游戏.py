990931f9513d4e6689aecc6c2f0dd2abc01f9146
HouQi9475 <573298496@qq.com>
2017-08-31 23:30:21 +0800

    python..

---
+++ python入门2_猜数字游戏.py
@@ -0,0 +1,11 @@
+print ('------侯琪学python!-------')
+temp = input ('猜猜我心里想的数字:')
+guess = int (temp)
+if guess ==3:
+    print ('卧槽，牛逼啊！')
+    print ('厉害了我的哥!')
+else:
+    print ('你个渣渣!')
+print ('game over..')
+name=input ('请输入姓名:')
+print ('你好'+name)
